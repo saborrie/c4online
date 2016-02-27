1e79bd45365f43a9301d6f97f6bf02a84caa1035
Steven Borrie <saborrie@gmail.com>
2016-02-27 01:44:43 +0000

    initial changes

--- index.js
+++ index.js
@@ -1,20 +1,85 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+// Socket.IO part
+var io = require('socket.io')(server);
 
+board = [];
+for(var i=0; i<6; i++) {
+    board[i] = [];
+    for(var j=0; j<7; j++) {
+        board[i][j] = "E";
+    }
+}
+
+var users = [];
+var usercounter = 0;
+turn = "red";
+
+// var sendComments = function (socket) {
+//     fs.readFile('_comments.json', 'utf8', function(err, comments) {
+//         comments = JSON.parse(comments);
+//         socket.emit('comments', comments);
+//     });
+// };
+
+var sendBoard = function (socket) {
+    io.emit('board', board);
+    // socket.broadcast.emit('board', board);
+};
+
+io.on('connection', function (socket) {
+    console.log('New client connected!');
+
+    user = {
+        id: usercounter++,
+        socket: socket
+    };
+    users.push(user);
+
+    
+    socket.on('fetch', function () {
+        sendBoard(socket);
+    });
+  
+    socket.on('newMove', function (move, callback) {
+
+        for(var row=0; row<6; row++) {
+            if(board[row][move.column] == "E") {
+                board[row][move.column] = turn;
+                sendBoard(socket);
+                turn = turn=="red" ? "yellow" : "red";
+                break;
+            }
+        }
+    });
+
+    socket.on('disconnect', function(){
+        users.pop(user);
+    });
+
+    // socket.on('fetchComments', function () {
+    //     sendComments(socket);
+    // });
+
+    // socket.on('newComment', function (comment, callback) {
+    //     fs.readFile('_comments.json', 'utf8', function(err, comments) {
+    //         comments = JSON.parse(comments);
+    //         comments.push(comment);
+    //         fs.writeFile('_comments.json', JSON.stringify(comments, null, 4), function (err) {
+    //             io.emit('comments', comments);
+    //             callback(err);
+    //         });
+    //     });
+    // });
+});
---
+++ public/app.jsx
@@ -0,0 +1,151 @@
+var App = React.createClass({
+    getInitialState: function () {
+        return {
+            board: null,
+            messages: []
+        };
+    },
+    componentDidMount: function () {
+        var that = this;
+        this.socket = io();
+        this.socket.on('board', function (board) {
+            that.setState({ board: board });
+        });
+        this.socket.on('messageChange', function (messages) {
+            that.setState({ messages: messages });
+        });
+        this.socket.emit('fetch');
+    },
+    sendMessage: function (message, callback) {
+        this.socket.emit('newMessage', message, function (err) {
+            if (err)
+                return console.error('New message error:', err);
+            callback();
+        });
+    },
+    makeMove: function (column, callback) {
+        this.socket.emit('newMove', {column: column}, function (err) {
+            if (err)
+                return console.error('New move error:', err);
+            callback();
+        });
+    },
+    render: function() {
+        return (
+            <div className="app">
+                <Board board={this.state.board} makeMove={this.makeMove}/>
+            </div>
+        );
+
+        // <ChatSection messages={this.state.messages} sendMessage={this.sendMessage}/>
+    }
+});
+
+
+var Board = React.createClass({
+    render: function () {
+        var board = (<div>Loading board...</div>);
+        var that = this;
+
+        if(this.props.board) {
+
+            board = [];
+
+            oc = function(c){ return function() { that.props.makeMove(c); }; };
+
+            for(var row=0; row<6; row++) {
+                for(var column=0; column<7; column++) {
+                    board.unshift(<Hole clicked={oc(column)} colour={this.props.board[row][column]}/>);
+                }
+            }
+        }
+
+        return (
+            <div className="board">
+                {board}
+            </div>
+        );
+    }
+});
+
+var Hole = React.createClass({
+    handleClick: function () {
+        this.props.clicked();
+    },
+    render: function () {
+        return (
+            <div onClick={this.handleClick} className="hole"><div className={this.props.colour}></div></div>
+        );
+    }
+});
+
+// var ChatSection = React.createClass({
+//     render: function () {
+//         return (<div className="chatsection">
+//             <h3>Connect 4:</h3>
+//             <Board board={this.props.board} makeMove={this.makeMove}/>
+//             <ChatSection sendMessage={this.sendMessage}/>
+//         </div>);
+//     }
+// });
+
+
+// var MessageList = React.createClass({
+//     render: function () {
+//         var Comments = (<div>Loading comments...</div>);
+//         if (this.props.comments) {
+//             Comments = this.props.comments.map(function (comment) {
+//                 return (<Comment comment={comment} />);
+//             });
+//         }
+//         return (
+//             <div className="commentList">
+//                 {Comments}
+//             </div>
+//         );
+//     }
+// });
+
+
+// var Comment = React.createClass({
+//     render: function () {
+//         return (
+//             <div className="comment">
+//                 <span className="author">{this.props.comment.author}</span> said:<br/>
+//                 <div className="body">{this.props.comment.text}</div>
+//             </div>
+//         );
+//     }
+// });
+// var CommentForm = React.createClass({
+//     handleSubmit: function (e) {
+//         e.preventDefault();
+//         var that = this;
+//         var author = this.refs.author.getDOMNode().value;
+//         var text = this.refs.text.getDOMNode().value;
+//         var comment = { author: author, text: text };
+//         var submitButton = this.refs.submitButton.getDOMNode();
+//         submitButton.innerHTML = 'Posting comment...';
+//         submitButton.setAttribute('disabled', 'disabled');
+//         this.props.submitComment(comment, function (err) {
+//             that.refs.author.getDOMNode().value = '';
+//             that.refs.text.getDOMNode().value = '';
+//             submitButton.innerHTML = 'Post comment';
+//             submitButton.removeAttribute('disabled');
+//         });
+//     },
+//     render: function () {
+//         return (
+//             <form className="commentForm" onSubmit={this.handleSubmit}>
+//                 <input type="text" name="author" ref="author" placeholder="Name" required /><br/>
+//                 <textarea name="text" ref="text" placeholder="Comment" required></textarea><br/>
+//                 <button type="submit" ref="submitButton">Post comment</button>
+//             </form>
+//         );
+//     }
+// });
+
+React.render(
+    <App/>,
+    document.getElementById('content')
+);
