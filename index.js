df00da50c62c2608812888006054cf57afa6b6b0
Steven Borrie <saborrie@gmail.com>
2016-02-27 01:46:35 +0000

    fixed var server line

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
